afea10c9d28f774e08fcbbb66687b9632471dd27
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-08-17 02:00:54 +0900

    - feature: add User, Auth, and Redis modules to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { DatabaseModule } from "./database/database.module";
 import { ConfigModule } from "./config/config.module";
+import { UserModule } from "@src/user/user.module";
+import { AuthModule } from "@src/auth/auth.module";
+import { RedisModule } from "@src/redis/redis.module";
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, UserModule, AuthModule, RedisModule],
   controllers: [],
   providers: [],
 })
