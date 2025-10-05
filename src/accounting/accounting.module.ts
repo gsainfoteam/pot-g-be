0fffa46b5fb75bf9ce07be59dc2b06554cbb9812
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-10-06 01:44:31 +0900

    - fix: AccountingModule import bug 수정

--- src/accounting/accounting.module.ts
+++ src/accounting/accounting.module.ts
@@ -4,9 +4,10 @@ import { AccountingService } from "@src/accounting/accounting.service";
 import { AccountingController } from "@src/accounting/accounting.controller";
 import { PotModule } from "@src/pot/pot.module";
 import { BroadcastingModule } from "@src/broadcasting/broadcasting.module";
+import { PopoModule } from "@src/popo/popo.module";
 
 @Module({
-  imports: [DatabaseModule, PotModule, BroadcastingModule],
+  imports: [DatabaseModule, PotModule, BroadcastingModule, PopoModule],
   providers: [AccountingService],
   controllers: [AccountingController],
 })
