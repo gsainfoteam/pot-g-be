db2ec854aa184e84ad061f87633970af20af0b64
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-10-05 21:24:56 +0900

    - feature: sendPopoChatMsgToPotRoom 구현

--- src/popo/popo.service.ts
+++ src/popo/popo.service.ts
@@ -5,14 +5,24 @@ import { PopoChatReservationRepository } from "@src/database/repository/popo-cha
 import { BroadcastingService } from "@src/broadcasting/broadcasting.service";
 import { PotService } from "@src/pot/pot.service";
 import { Pot } from "@src/pot/model/pot";
+import { PotPopoChatEventV1 } from "@src/pot/event/v1/pot-popo-chat-event";
+import { PotEventReducer } from "@src/pot/event/pot-event-reducer";
+import { PotEventError } from "@src/global/exceptions/pot-event.error";
+import { getUnixTime } from "date-fns";
+import { TxType } from "@src/global/types/tx.types";
+import { DatabaseService } from "@src/database/database.service";
+import { PotEventRepository } from "@src/database/repository/pot-event.repository";
+import { PopoChatStringType } from "../../drizzle/schema/popo-chat-msg";
 
 @Injectable()
 export class PopoService implements OnModuleInit {
   private cachedPopoChatMsgs: PopoChatMsgEntity[] = [];
 
   constructor(
+    private readonly dbService: DatabaseService,
     private readonly popoChatMsgRepository: PopoChatMsgRepository,
     private readonly popoChatReservationRepository: PopoChatReservationRepository,
+    private readonly potEventRepository: PotEventRepository,
     private readonly broadcastingService: BroadcastingService,
     @Inject(forwardRef(() => PotService))
     private readonly potService: PotService,
@@ -33,9 +43,9 @@ export class PopoService implements OnModuleInit {
   ) {}
 
   async sendPopoChatMsgToPotRoom(
+    popoChatMsg: PopoChatMsgEntity,
     potPk?: string,
     pot?: Pot,
-    popoChatMsg: PopoChatMsgEntity,
   ) {
     if (!pot && !potPk) {
       throw new Error("Either pot or potPk must be provided");
@@ -43,6 +53,47 @@ export class PopoService implements OnModuleInit {
     if (!pot && potPk) {
       pot = await this.potService.getPot(potPk);
     }
+
+    if (!pot) {
+      throw new Error("Pot not found");
+    }
+
+    const now = new Date();
+
+    const potPopoChatMsgEvent: PotPopoChatEventV1 =
+      PotPopoChatEventV1.generateEvent(pot.pk, now, {
+        popoChatType: popoChatMsg.type,
+        message: popoChatMsg.message,
+        actionBtns: popoChatMsg.actionBtns,
+        timestamp: now,
+      });
+
+    try {
+      PotEventReducer.reduce(pot, potPopoChatMsgEvent, true);
+    } catch (error) {
+      if (error instanceof PotEventError) {
+        return error.baseResultDto;
+      }
+      throw error;
+    }
+
+    await this.dbService.db.transaction(async (tx: TxType) => {
+      await this.potEventRepository.saveEvent(potPopoChatMsgEvent, tx);
+    });
+
+    this.broadcastingService.asyncBroadcastPotEvent(
+      {
+        pot_pk: pot.pk,
+        timestamp: getUnixTime(potPopoChatMsgEvent.timestamp),
+        event_type: potPopoChatMsgEvent.eventType,
+        data: potPopoChatMsgEvent.toDto(),
+      },
+      pot.joinedUserPks,
+    );
+  }
+
+  getPopoChatMsgByType(type: PopoChatStringType): PopoChatMsgEntity | null {
+    return this.cachedPopoChatMsgs.find((msg) => msg.type === type) || null;
   }
 
   private async cachePopoChatMsgs() {
