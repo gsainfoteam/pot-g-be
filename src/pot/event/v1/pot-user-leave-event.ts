476b922bf0c6fd9584e957bd5640cbe412be7e44
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-10-11 16:15:43 +0900

    - fix: pot event rest 조회 시 pot user leave event 에 host_changed_to 필드 안오는 버그 수정

--- src/pot/event/v1/pot-user-leave-event.ts
+++ src/pot/event/v1/pot-user-leave-event.ts
@@ -13,6 +13,7 @@ import { PotEventUserLeaveV1Dto } from "@src/pot/event/v1/dto/pot-event.user-lea
 export type PotUserLeaveEventV1Schema = {
   potRoomPk: string; // 퇴장할 방의 ID
   userPk: string; // 퇴장할 유저의 ID
+  host_changed_to?: string; // 방장이 변경된 경우 변경된 방장 ID (dispatch 에 의해서만 생성됩니다.)
 };
 
 export class PotUserLeaveEventV1
@@ -46,7 +47,7 @@ export class PotUserLeaveEventV1
     // 퇴장한 유저가 방장인 경우 방장 선출
     if (pot.hostUserPk === data.userPk && pot.joinedUserPks.length > 0) {
       pot.hostUserPk = pot.joinedUserPks[0];
-      this.host_changed_to = pot.hostUserPk;
+      this.data.host_changed_to = pot.hostUserPk;
     }
 
     return pot;
@@ -72,11 +73,9 @@ export class PotUserLeaveEventV1
   toDto(): PotEventUserLeaveV1Dto {
     return {
       user_pk: this.data.userPk,
-      host_changed_to: this.host_changed_to || undefined,
+      host_changed_to: this.data.host_changed_to || undefined,
     };
   }
-
-  private host_changed_to?: string;
 
   readonly potRoomPk: string;
   readonly eventType: PotEventStringType;
