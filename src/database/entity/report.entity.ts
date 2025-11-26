d5ae577dbca033fb4edb6f746f0e2ee299096b8b
YeonhyukKim <brankein13@gm.gist.ac.kr>
2025-11-26 19:45:39 +0900

    - feature: report 테이블 추가

---
+++ src/database/entity/report.entity.ts
@@ -0,0 +1,15 @@
+import { UserEntity } from "@src/database/entity/user.entity";
+import { PotRoomEntity } from "@src/database/entity/pot-room.entity";
+
+export class ReportEntity {
+  pk: string;
+  potRoomFk: string;
+  potRoomEntity?: PotRoomEntity;
+  userFk: string;
+  userEntity?: UserEntity;
+  targetUserFk: string;
+  targetUserEntity?: UserEntity;
+  reason: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
