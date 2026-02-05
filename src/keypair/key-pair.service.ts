02fc461ecbbd3311cb8bebc1fc88ce077ff6339f
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-02-06 02:01:02 +0900

    - feat: implement JWT key pair generation for initial setting

--- src/keypair/key-pair.service.ts
+++ src/keypair/key-pair.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { generateKeyPairSync } from "node:crypto";
 import { JwtKeyPairRepository } from "@src/database/repository/jwt-key-pair.repository";
 
 @Injectable()
@@ -10,10 +11,23 @@ export class KeyPairService {
 
   async getKeyPair() {
     if (!this.jwtPublicKey || !this.jwtPrivateKey) {
-      const { publicKey, privateKey } =
-        await this.jwtKeyPairRepository.getKeyPair();
-      this.jwtPublicKey = publicKey;
-      this.jwtPrivateKey = privateKey;
+      const keyPair = await this.jwtKeyPairRepository.getKeyPair();
+
+      if (keyPair) {
+        this.jwtPublicKey = keyPair.publicKey;
+        this.jwtPrivateKey = keyPair.privateKey;
+      } else {
+        const { publicKey, privateKey } = generateKeyPairSync("rsa", {
+          modulusLength: 2048,
+          publicKeyEncoding: { type: "spki", format: "pem" },
+          privateKeyEncoding: { type: "pkcs8", format: "pem" },
+        });
+
+        await this.jwtKeyPairRepository.saveKeyPair(publicKey, privateKey);
+
+        this.jwtPublicKey = publicKey;
+        this.jwtPrivateKey = privateKey;
+      }
     }
     return {
       publicKey: this.jwtPublicKey,
